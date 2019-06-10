e851bb269f16696e67278fc7eca39974f67ca20f
zxl <870362757@qq.com>
2019-06-10 17:35:24 +0800

    zxl 头部

--- src/router.js
+++ src/router.js
@@ -20,6 +20,62 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    {
+      path: '/vip',
+      name: 'vip',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Vip.vue'),
+    },
+    {
+      path: '/lateList',
+      name: 'lateList',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/LateList.vue'),
+    },
+    {
+      path: '/cmt/',
+      name: 'cmt',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Cmt.vue'),
+    },
+    {
+      path: '/category/',
+      name: 'category',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Category.vue'),
+    },
+    {
+      path: '/my/',
+      name: 'my',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/My.vue'),
+    },
+    {
+      path: '/sheets/',
+      name: 'sheets',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Sheets.vue'),
+    },
+    {
+      path: '/search/',
+      name: 'search',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Search.vue'),
+    },
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <SfHeader/>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SfHeader from '@/components/SfHeader.vue'; // @ is an alias to /src
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    data(){
+        return{
+
+        }
+    },
+    components:{
+        SfHeader
+    }
 }
 </script>
+
+<style>
+html{
+  font-size: 117.1875px;
+}
+body {
+    background-color: #F3F3F3;
+    margin: 0;
+    font-family: Helvetica;
+    -webkit-text-size-adjust: 100%;
+    font-size: 0.12rem;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+</style>
